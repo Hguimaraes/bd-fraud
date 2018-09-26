4b2a25da4dfbc7005152149fb3d78fa98e59eea9
Hguimaraes <hguimaraes@poli.ufrj.br>
2018-09-25 22:44:53 -0300

    removing argparse to make easy on spark submit

--- src/server.py
+++ src/server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import argparse
 import os
 import sys
 import time
@@ -40,21 +39,9 @@ def run_server(app, port):
  
  
 if __name__ == "__main__":
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Fraud detection system using Big Data Technologies')
-
-    # Required argument
-    parser.add_argument('-p', '--port', help='Port to deploy the solution', type=str, required=True)
-    parser.add_argument('-d', '--data', help='Path to dataset in CSV format to stream to the solution',
-        type=str, required=True)
-    args = parser.parse_args()
-
-    # Pass args, activate logging and start the main function
-    args = parser.parse_args()
-
     # Init spark context and load libraries
     sc = init_spark_context()
-    app = create_app(sc, args.data)
+    app = create_app(sc, '/user/ita-fds/')
  
     # start web server
-    run_server(app, args.port)
+    run_server(app, 8000)
